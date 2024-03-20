bf7886547eb5c270e036ff1c1169d242b53bf4a5
annihong <annihong7153@gmail.com>
2024-03-20 17:36:08 -0400

    still need to work on obtaining a df of n x T+1 of the cluster being first treated at time T

--- R/estimator.R
+++ R/estimator.R
@@ -1,20 +1,5 @@
 # Description: This file contains the functions to estimate the parameters of the model
 
-# returns the weights on each ci, k periods after treatment. if observed is true, then df = df_observed containing variable: "time", "id", "cluster", "g", "cluster_min", "Y_obs", it returns the DiD weights for the observed data where Gmin_ci < T are in the control group. If observed is false, then df = df_long, the true data, containing variable: "time", "id", "cluster", "g", "cluster_min", "Y_G", TE, "P_Gmin" it returns the weights on the TE_ci of the random version of the true estimator theta_k_weights. 
-theta_k_weights <- function(df, fixed_res, k, df_prob_cluster_g, observed=TRUE, ...) {
-    never_trt_indices <- 
-    if (observed) {
-        df <- df_observed
-    }
-    else {
-        df <- df_long
-    }
-
-
-}
-
-
-
 tilde_k_g <- function(df, fixed_res, k,g, df_prob_cluster_g, t_total) {
     treated_df <- df[df$cluster_gmin <= t_total,]
     n_trt <- nrow(treated_df[treated_df$time==0,]) 
